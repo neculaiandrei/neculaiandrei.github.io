5502d0f4ba6085fcacb3825aa50ed38996cba710
neculaiandrei <an_drey_curent@yahoo.com>
2021-11-15 21:11:10 +0200

    more

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ const theme = document.querySelector(".home__theme");
 theme.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+
+
+const moreButtons = document.querySelectorAll(".experience__more");
+
+moreButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const extra = btn.parentNode.querySelector(".experience__extra");
+    const extraDisplay = extra.style.display ? extra.style.display : window.getComputedStyle(extra, null).getPropertyValue('display');
+
+    if (extraDisplay === 'none') {
+      extra.style.display = 'block';
+      btn.innerHTML = 'Less';
+    } else {
+      extra.style.display = 'none';
+      btn.innerHTML = 'More';
+    }
+  });
+})
